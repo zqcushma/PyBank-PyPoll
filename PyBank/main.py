8c7b8f7420d48ef5c9b39b6549b44bf3166f4126
@zqcushma <zqcushma@mtu.edu>
2023-09-28 19:39:19 -0400

    pybank needs calculation clarification, pypoll is 852 votes over

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,38 @@
-import pandas as pdr
+import csv
  
 #filepath is hardcoded due to issues with writing a relative reference
-filePath = "D:/Desktop/Data Analytics Work/Week 3/PyBank-PyPoll/PyBank/Resources/budget_data.csv"
-data = pdr.read_csv(filePath)
-
-#total months in set
-totalMonths = len(data["Date"])
-row = 0
+readFilePath = "D:/Desktop/Data Analytics Work/Week 3/PyBank-PyPoll/PyBank/Resources/budget_data.csv"
+writeFilePath = "D:/Desktop/Data Analytics Work/Week 3/PyBank-PyPoll/PyBank/analysis/results.txt"
+#filePath = 'Resources/budget_data.csv'
+#row 0 is header, each row after is an entry into the table
+totalMonths = 0
+totalChange = 0
 bigHigh = ["", 0]
 bigLow = ["", 0]
+with open(readFilePath) as file:
+    line = 0
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        if line == 0:
+            headers = row
+            line += 1
+        else:
+            totalMonths += 1.0
+            print(row[1])
+            totalChange += int(row[1])
+            #greatest increase in price
+            if int(bigHigh[1]) < int(row[1]):
+                bigHigh = [row[0], row[1]]
+            #greatest decrease in price
+            if int(bigLow[1]) > int(row[1]):
+                bigLow = [row[0], row[1]]
 
-totalChange = sum(data["Profit/Losses"])
-averageChange = totalChange/len(data["Profit/Losses"])
-for dataPoint in data["Profit/Losses"]:
-    
-    #greatest increase in price
-    if bigHigh[1] < dataPoint:
-        bigHigh = [data["Date"][row], dataPoint]
-    #greatest decrease in price
-    if bigLow[1] > dataPoint:
-        bigLow = [data["Date"][row], dataPoint]
-    row += 1
+#THIS PORTION MAY BE WRONG, UNKNOWN WHAT IS REQUIRED HERE
+averageChange = totalChange/totalMonths
+averageChange = round(averageChange,2)
 
-    #changes in +/- over full period and the average of those changes
-    #changes over the course of the period, with average given
-    
+output = f"Financial Analysis\n----------------------------\nTotal Months: {totalMonths}\nTotal: ${totalChange}\nAverage Change: ${averageChange}\nGreatest Increase in Profits: {bigHigh[0]} with ${bigHigh[1]}\nGreatest Decrease in Profits: {bigLow[0]} with ${bigLow[1]}"
+print(output)
+
+with open(writeFilePath, 'w') as file:
+    file.write(output)
---
+++ PyPoll/main.py
@@ -0,0 +1,61 @@
+import csv
+ 
+#filepath is hardcoded due to issues with writing a relative reference
+readFilePath = "D:/Desktop/Data Analytics Work/Week 3/PyBank-PyPoll/PyPoll/Resources/election_data.csv"
+writeFilePath = "D:/Desktop/Data Analytics Work/Week 3/PyBank-PyPoll/PyPoll/analysis/results.txt"
+#row 0 is header, each row after is an entry into the table
+line = 0
+
+#declaring variables for tracking
+currentCanidate = ""
+#vote values are off of the canidate list array by 1
+canidateResults = dict()
+canidateVotes = 0
+totalVotes = 0
+#open file
+with open(readFilePath) as file:
+    headers = next(file)
+    reader = csv.reader(file, delimiter=',')
+    #loop through and calculate how many votes each canidate got, along with adding up the total number of votes
+    
+    firstEntry = True
+    for row in reader:
+        if firstEntry:
+            currentCanidate = row[2]
+            firstEntry = False
+        elif currentCanidate != row[2]:
+            if currentCanidate in canidateResults:
+                canidateResults[currentCanidate] += canidateVotes
+            else:
+                canidateResults.update({currentCanidate : canidateVotes})
+            canidateVotes = 0
+            currentCanidate = row[2]
+                
+        canidateVotes += 1
+        totalVotes += 1
+
+#print(str(canidateResults))
+#print(totalVotes)
+#identify winner
+winner = ""
+winningVotes = 0
+#winner for loop
+for canidate in canidateResults:
+    if canidateResults[canidate] > winningVotes:
+        winner = canidate
+        winningVotes = canidateResults[canidate]
+
+#calculate percentage won and store in string for output
+allResults = ""
+percentWon = 0.0
+for canidate in canidateResults:
+    percentWon = round((canidateResults[canidate]/totalVotes)*100, 3)
+    allResults += f"{canidate}: {percentWon}% ({canidateResults[canidate]})\n"
+#string builder for loop
+
+
+output = f"Election Results\n-------------------------\nTotal Votes: {totalVotes}\n-------------------------\n{allResults}-------------------------\nWinner: {winner}\n-------------------------"
+print(output)
+
+with open(writeFilePath, 'w') as file:
+    file.write(output)
