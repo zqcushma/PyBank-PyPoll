fac594587c0bad2df75896ca3476b8d5f571ee3a
@zqcushma <zqcushma@mtu.edu>
2023-09-28 21:21:07 -0400

    identified what i needed to do for an assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,27 +9,36 @@ totalMonths = 0
 totalChange = 0
 bigHigh = ["", 0]
 bigLow = ["", 0]
+plList = []
 with open(readFilePath) as file:
     line = 0
+    headers = next(file)
     reader = csv.reader(file, delimiter=',')
     for row in reader:
-        if line == 0:
-            headers = row
-            line += 1
-        else:
-            totalMonths += 1.0
-            print(row[1])
-            totalChange += int(row[1])
-            #greatest increase in price
-            if int(bigHigh[1]) < int(row[1]):
-                bigHigh = [row[0], row[1]]
-            #greatest decrease in price
-            if int(bigLow[1]) > int(row[1]):
-                bigLow = [row[0], row[1]]
+
+        totalMonths += 1.0
+        #print(row[1])
+        totalChange += int(row[1])
+        #greatest increase in price
+        if int(bigHigh[1]) < int(row[1]):
+            bigHigh = [row[0], row[1]]
+        #greatest decrease in price
+        if int(bigLow[1]) > int(row[1]):
+            bigLow = [row[0], row[1]]
+        plList.append(row[1])
+            
 
 #THIS PORTION MAY BE WRONG, UNKNOWN WHAT IS REQUIRED HERE
-averageChange = totalChange/totalMonths
-averageChange = round(averageChange,2)
+averageChange = 0.0
+changes = [0]
+for entry in plList:
+    prevEntry = plList.index(entry) - 1
+    if prevEntry < 0:
+        continue
+    else:
+        changes.append(float(entry) - float(plList[prevEntry]))
+
+averageChange = round(sum(changes)/len(changes),2)
 
 output = f"Financial Analysis\n----------------------------\nTotal Months: {totalMonths}\nTotal: ${totalChange}\nAverage Change: ${averageChange}\nGreatest Increase in Profits: {bigHigh[0]} with ${bigHigh[1]}\nGreatest Decrease in Profits: {bigLow[0]} with ${bigLow[1]}"
 print(output)
